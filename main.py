786c1d078b204d68ef8f47795d9559b05a411c06
Jacob Sharon <jacob012@live.missouristate.edu>
2025-06-30 13:19:33 -0500

    Added tree generation in main.py. Added prototype function to visualize and save tree images.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from genetic_programming.primitives import primitive_set
+from deap import gp
+import matplotlib.pyplot as plt
+import networkx as nx
+import pygraphviz as pgv
+from explain.tree_visualization import visualize_tree
 
 def main():
-    print("Aruments:")
-    for i, name in enumerate(primitive_set.arguments):
-        print(f"ARG{i} : {name}")
+    expr = gp.genFull(primitive_set, min_=3, max_=8)
+    tree = gp.PrimitiveTree(expr)
+    visualize_tree(tree)
 
 if __name__ == "__main__":
     main()
